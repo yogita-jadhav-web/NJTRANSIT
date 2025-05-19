d4c3260435d72ee53d335fc155de071e02dc0263
52292663 <52292663@COREPW0CFJMM>
2025-05-19 14:12:07 +0530

    added sanity suite by Rashmi on 19th May

---
+++ Test Suites/Sanity_Suite.ts
@@ -0,0 +1,98 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Sanity_Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>d72e759c-afb4-4045-9c07-847260fbe077</testSuiteGuid>
+   <testCaseLink>
+      <guid>aba1db4c-6098-41a9-9d0d-29b4fe6df033</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/x.com.njt.field.validations.testcases/TC_67882_To_verify_the_new_user_registration</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>cfa6091c-9ca4-418c-a2ca-419898924c66</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/z.com.njt.testcases/Fare_Pay_Card_TCs/109178_To_verify_that_user_is_able_to_register_an_active fare_pay_card</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>19d57226-01f7-4a82-b867-4709385880b0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/z.com.njt.testcases/Fare_Pay_Card_TCs/TC_109180_To_verify_user_is_able_to_unregister_fare_pay_card</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8a5f6212-30a4-472f-8c07-e6edb341a87f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/z.com.njt.testcases/Fare_Pay_Card_TCs/TC_109188_To_verify_that_user_is_able_to_Add_Value_using_the_existing_VISA_credit_card</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8f864081-7e01-4221-8085-572a72bd6a96</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/z.com.njt.testcases/Fare_Pay_Card_TCs/TC_109189_To_verify_that_user_is_able_to_Add_Value_using_with_add_new_VISA_card_without_save</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>782cbcfe-7915-404e-bf2c-68c51b40e4fb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/z.com.njt.testcases/Fare_Pay_Card_TCs/TC_109190_To_verify_that_user_is_able_to_Add_Value_using_with_add_new_VISA_card_with_save</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0694994c-8dbd-4c68-8b35-cf201f09729d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/z.com.njt.testcases/Fare_Pay_Card_TCs/TC_109221_To_Verify_that_user_is_able_to_purchase_Rail_monthly_with_existing_Amex_credit_card</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1ebb7c7e-9b3f-4dd6-a302-1d0efea6345a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/z.com.njt.testcases/Fare_Pay_Card_TCs/TC_109292_To_Verify_that_user_is_able_to_purchase_Bus_10Trip_Ticket_while_adding_new_Master_card_and_save</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>017a052a-9559-4807-ab56-7759ea36b7b7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/z.com.njt.testcases/Fare_Pay_Card_TCs/TC_109321_To_Verify_that_user_is_able_to_purchase_NLR_monthly_ticket_while_adding_new_CUP_card</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>aa9dbfa3-cee4-45aa-a120-d80caec7808c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/z.com.njt.testcases/Refunds_Portal_TCs/Verification of Full Refund for Bus 10 Trip-38</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dfb6ebf7-5b20-46fe-b11c-21a335e46a3b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/z.com.njt.testcases/Auto_Reload_Tickets_TCs/TC_147097_To verify_that_user_is_able_to_subscribe_HBLR_monthly_AutoReload_Tickets_using_Existing_VISA_card</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>52b2fe5b-0b9d-4a7f-9fca-2ecf6a3f2430</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/z.com.njt.testcases/Refunds_Portal_TCs/Verification of Full Refund for Rail 10 Trip-41</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
