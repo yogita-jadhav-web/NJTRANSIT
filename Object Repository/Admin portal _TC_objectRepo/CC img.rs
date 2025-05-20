cf1ab7c8f51b9251d9ab899a979ac1397f303f72
52292663 <52292663@COREPW0CFJMM.lan>
2025-05-20 12:12:15 +0530

    Updated the Reload Ticket TC by Rashmi on 20th May

---
+++ Object Repository/Admin portal _TC_objectRepo/CC img.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>CC img</name>
+   <tag></tag>
+   <elementGuidId>0ab9f35c-5c4a-4f08-a6b7-e138fb8314aa</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//img[@id='undefined_0']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
